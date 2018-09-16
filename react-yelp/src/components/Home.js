d97455e793093d786deaed6908ef03bd6aa7f3e0
vutsalsinghal <vutsalsinghal@gmail.com>
2018-09-16 13:21:32 -0400

    Minor change

--- react-yelp/src/components/Home.js
+++ react-yelp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card, Grid, Loader, Dimmer} from 'semantic-ui-react';
+import {Card, Grid, Loader, Dimmer, Form, Button} from 'semantic-ui-react';
 import axios from 'axios';
 
 class Home extends Component {
@@ -8,6 +8,8 @@ class Home extends Component {
     events:[],
     display_limit:'',
     total:'',
+    displayed_events:'',
+    errorMessage:'',
   }
 
   async componentDidMount(){
@@ -18,7 +20,6 @@ class Home extends Component {
 
     let response = await axios.get('https://eastvillage.pythonanywhere.com/json/events',config);
     this.setState({events:response.data.events, total:response.data.total, display_limit:response.data.display_limit});
-
     this.setState({loadingData:false});
   }
 
@@ -41,6 +42,21 @@ class Home extends Component {
     return <div>{items}</div>;
   }
 
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    var config = {
+      display:this.state.displayed_events,
+    }
+
+    let response = await axios.post('https://eastvillage.pythonanywhere.com/json/events', config);
+    console.log(response.data);
+  }
+
+  handleChange = event => {
+    this.setState({ displayed_events: event.target.value });
+  }
+
   render() {
     if(this.state.loadingData){
       return (
@@ -53,14 +69,20 @@ class Home extends Component {
     return (
       <div>
         <h1>Events around East Village!</h1>
+        <i>Click on any card to explore</i>
         <Grid stackable reversed="mobile">
           <Grid.Column width={12}>
             {this.rederEvents()}
             <i>Displaying {this.state.display_limit}/{this.state.total}</i>
           </Grid.Column>
           <Grid.Column width={4}>
-            <Grid.Row>
-            </Grid.Row>
+            {/*<Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label>Displaying</label>
+                <input placeholder='No. Of Events' onChange={this.handleChange}  />
+              </Form.Field>
+              <Button primary type='submit'>Submit</Button>
+            </Form>*/}
           </Grid.Column>
         </Grid>
       </div>
